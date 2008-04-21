13890bc5744f76c5f9e1eb28ae945cac82581a7f
thetrueaplus <thetrueaplus@a6c76449-3c4b-0410-9e40-efb2f4630036>
2008-04-21 16:39:07 +0000

    added some http headers

---
+++ http.h
@@ -0,0 +1,36 @@
+//
+// Mountain CMS
+// Daniel A. White
+//
+// http.h
+// Stores the HTTP constants
+
+#ifndef HTTP_H
+#define HTTP_H
+
+//Numerical status codes
+
+#define MTN_CMS_HTTP_CONTINUE                        100
+#define MTN_CMS_HTTP_SWITCHING_PROTOCOLS             101
+#define MTN_CMS_HTTP_OK                              200
+#define MTN_CMS_HTTP_CREATED                         201
+#define MTN_CMS_HTTP_ACCEPTED                        202
+#define MTN_CMS_HTTP_NON_AUTHORITATIVE_INFORMATION   203
+#define MTN_CMS_HTTP_NO_CONTENT                      204
+#define MTN_CMS_HTTP_RESET_CONTENT                   205
+#define MTN_CMS_HTTP_PARTIAL_CONTENT                 206
+#define MTN_CMS_HTTP_MULTIPLE_CHOICES                300
+#define MTN_CMS_HTTP_MOVED_PERMANENTLY               301
+#define MTN_CMS_HTTP_FOUND                           302
+#define MTN_CMS_HTTP_SEE_OTHER                       303
+#define MTN_CMS_HTTP_NOT_MODIFIED                    304
+#define MTN_CMS_HTTP_USE_PROXY                       305
+#deinne MTN_CMS_HTTP_TEMPORARY_REDIRECT              307
+#define MTN_CMS_HTTP_BAD_REQUEST                     400
+#define MTN_CMS_HTTP_UNAUTHORIZED                    401
+#define MTN_CMS_HTTP_PAYMENT_REQUIRED                402
+#define MTN_CMS_HTTP_FORBIDDEN                       403
+#define MTN_CMS_HTTP_NOT_FOUND                       404
+#define MTN_CMS_
+
+#endif
