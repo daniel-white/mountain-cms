bdaf5268aab492a70bcab662a76b2c8a4c5eb63c
thetrueaplus <thetrueaplus@a6c76449-3c4b-0410-9e40-efb2f4630036>
2008-04-21 23:06:03 +0000

    more constants defined

--- http.h
+++ http.h
@@ -10,12 +10,14 @@
 
 // Methods
 
+#define MTN_CMS_HTTP_METHOD_CONNECT                         "CONNECT"
 #define MTN_CMS_HTTP_METHOD_DELETE                          "DELETE"
 #define MTN_CMS_HTTP_METHOD_GET                             "GET"
 #define MTN_CMS_HTTP_METHOD_HEAD                            "HEAD"
 #define MTN_CMS_HTTP_METHOD_OPTIONS                         "OPTIONS"
 #define MTN_CMS_HTTP_METHOD_POST                            "POST"
 #define MTN_CMS_HTTP_METHOD_PUT                             "PUT"
+#define MTN_CMS_HTTP_METHOD_TRACE                           "TRACE"
 
 // status codes as numbers
 
@@ -57,7 +59,7 @@
 #define MTN_CMS_HTTP_STATUS_REQUEST_RANGE_NOT_SATISFIABLE   416
 #define MTN_CMS_HTTP_STATUS_EXPECTATION_FAILED              417
 
-#define MTN_CMS_HTTP_STATUS_INTERNAL_SERVER_ERRROR          500
+#define MTN_CMS_HTTP_STATUS_INTERNAL_SERVER_ERROR           500
 #define MTN_CMS_HTTP_STATUS_NOT_IMPLEMENTED                 501
 #define MTN_CMS_HTTP_STATUS_BAD_GATEWAY                     502
 #define MTN_CMS_HTTP_STATUS_SERVICE_UNAVAILABLE             503
@@ -85,30 +87,30 @@
 #define MTN_CMS_HTTP_STATUS_USE_PROXY_S                     "305 Use Proxy"
 #define MTN_CMS_HTTP_STATUS_TEMPORARY_REDIRECT_S            "307 Temporary Redirect"
 
-#define MTN_CMS_HTTP_STATUS_BAD_REQUEST_S                   400
-#define MTN_CMS_HTTP_STATUS_UNAUTHORIZED_S                  401
-#define MTN_CMS_HTTP_STATUS_PAYMENT_REQUIRED_S              402
-#define MTN_CMS_HTTP_STATUS_FORBIDDEN_S                     403
-#define MTN_CMS_HTTP_STATUS_NOT_FOUND_S                     404
-#define MTN_CMS_HTTP_STATUS_METHOD_NOT_ALLOWED_S            405
-#define MTN_CMS_HTTP_STATUS_NOT_ACCEPTABLE_S                406
-#define MTN_CMS_HTTP_STATUS_PROXY_AUTHENICATION_REQUIRED_S  407
-#define MTN_CMS_HTTP_STATUS_REQUEST_TIME_OUT_S              408
-#define MTN_CMS_HTTP_STATUS_CONFLICT_S                      409
-#define MTN_CMS_HTTP_STATUS_GONE_S                          410
-#define MTN_CMS_HTTP_STATUS_LENGTH_REQUIRED_S               411
-#define MTN_CMS_HTTP_STATUS_PRECONDITION_FAILED_S           412
-#define MTN_CMS_HTTP_STATUS_REQUEST_ENTITY_TOO_LARGE_S      413
-#define MTN_CMS_HTTP_STATUS_REQUEST_URI_TOO_LARGE_S         414
-#define MTN_CMS_HTTP_STATUS_UNSUPPORTED_MEDIA_TYPE_S        415
-#define MTN_CMS_HTTP_STATUS_REQUEST_RANGE_NOT_SATISFIABLE_S 416
-#define MTN_CMS_HTTP_STATUS_EXPECTATION_FAILED_S            417
+#define MTN_CMS_HTTP_STATUS_BAD_REQUEST_S                   "400 Bad Request"
+#define MTN_CMS_HTTP_STATUS_UNAUTHORIZED_S                  "401 Unauthorized"
+#define MTN_CMS_HTTP_STATUS_PAYMENT_REQUIRED_S              "402 Payment Required"
+#define MTN_CMS_HTTP_STATUS_FORBIDDEN_S                     "403 Forbidden"
+#define MTN_CMS_HTTP_STATUS_NOT_FOUND_S                     "404 Not Found"
+#define MTN_CMS_HTTP_STATUS_METHOD_NOT_ALLOWED_S            "405 Not Allowed"
+#define MTN_CMS_HTTP_STATUS_NOT_ACCEPTABLE_S                "406 Not Acceptable"
+#define MTN_CMS_HTTP_STATUS_PROXY_AUTHENICATION_REQUIRED_S  "407 Authenication Required"
+#define MTN_CMS_HTTP_STATUS_REQUEST_TIME_OUT_S              "408 Request Time-out"
+#define MTN_CMS_HTTP_STATUS_CONFLICT_S                      "409 Conflict"
+#define MTN_CMS_HTTP_STATUS_GONE_S                          "410 Gone"
+#define MTN_CMS_HTTP_STATUS_LENGTH_REQUIRED_S               "411 Length Required"
+#define MTN_CMS_HTTP_STATUS_PRECONDITION_FAILED_S           "412 Precondition Required"
+#define MTN_CMS_HTTP_STATUS_REQUEST_ENTITY_TOO_LARGE_S      "413 Request Enity Too Large"
+#define MTN_CMS_HTTP_STATUS_REQUEST_URI_TOO_LARGE_S         "414 Request-URI Too Large"
+#define MTN_CMS_HTTP_STATUS_UNSUPPORTED_MEDIA_TYPE_S        "415 Unsupported Media Type"
+#define MTN_CMS_HTTP_STATUS_REQUEST_RANGE_NOT_SATISFIABLE_S "416 Requested range not satisfiable"
+#define MTN_CMS_HTTP_STATUS_EXPECTATION_FAILED_S            "417 Expectation Failed"
 
-#define MTN_CMS_HTTP_STATUS_INTERNAL_SERVER_ERRROR_S        500
-#define MTN_CMS_HTTP_STATUS_NOT_IMPLEMENTED_S               501
-#define MTN_CMS_HTTP_STATUS_BAD_GATEWAY_S                   502
-#define MTN_CMS_HTTP_STATUS_SERVICE_UNAVAILABLE_S           503
-#define MTN_CMS_HTTP_STATUS_GATEWAY_TIME_OUT_S              504
-#define MTN_CMS_HTTP_STATUS_HTTP_VERSION_NOT_SUPPORTED_S    505
+#define MTN_CMS_HTTP_STATUS_INTERNAL_SERVER_ERROR_S         "500 Internal Server Error"
+#define MTN_CMS_HTTP_STATUS_NOT_IMPLEMENTED_S               "501 Not Implemented"
+#define MTN_CMS_HTTP_STATUS_BAD_GATEWAY_S                   "502 Bad Gateway"
+#define MTN_CMS_HTTP_STATUS_SERVICE_UNAVAILABLE_S           "503 Service Unavailable"
+#define MTN_CMS_HTTP_STATUS_GATEWAY_TIME_OUT_S              "504 Gateway Time-out"
+#define MTN_CMS_HTTP_STATUS_HTTP_VERSION_NOT_SUPPORTED_S    "505 HTTP Version not supported"
 
 #endif
